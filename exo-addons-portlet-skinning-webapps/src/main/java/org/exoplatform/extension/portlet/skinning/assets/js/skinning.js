e1139944fd16d30e743044983b01dbd36f56c372
medamine <mkrout@exomplatform.com>
2015-10-13 13:05:50 +0100

    Add new fields

--- exo-addons-portlet-skinning-webapps/src/main/java/org/exoplatform/extension/portlet/skinning/assets/js/skinning.js
+++ exo-addons-portlet-skinning-webapps/src/main/java/org/exoplatform/extension/portlet/skinning/assets/js/skinning.js
@@ -6,6 +6,10 @@ $(document).ready(function () {
         var topHeadBgLight=".UIToolbarContainerLight .NormalContainerBlock .ToolbarContainer { background-color: "+$("#top_head_bg").val()+" ! important; background-image: none ! important;}";
         var topHeadBgDark=".UIToolbarContainerDark .NormalContainerBlock .ToolbarContainer { background-color: "+$("#top_head_bg").val()+" ! important; background-image: none ! important;}";
 
+        var topHeadBgHoLight=".UIToolbarContainerLight .uiDropdownWithIcon > a:hover{background:  "+$("#top_head_ho_bg").val()+" none repeat scroll 0 0 !important; color:  "+$("#top_head_ho_cl").val()+" !important; }";
+        var topHeadBgHoDark=".UIToolbarContainerDark .uiDropdownWithIcon > a:hover { background-color: "+$("#top_head_ho_bg").val()+" ! important; color: "+$("#top_head_ho_cl").val()+" !important; ! important;}";
+
+
         var  topTitleColorLight=".UIToolbarContainerLight .uiDropdownWithIcon > a { color: "+$("#top_head_cl").val()+" ! important;}";
         var  topTitleColorDark=".UIToolbarContainerDark .uiDropdownWithIcon > a { color: "+$("#top_head_cl").val()+" ! important;}";
 
@@ -14,8 +18,8 @@ $(document).ready(function () {
         var  title =".title  { background: "+$("#title_bg").val()+" none repeat scroll 0 0 !important; color: "+$("#title_cl").val()+" !important;}";
         var LeftNavigationTDContainer=".LeftNavigationTDContainer {   background: "+$("#l_nav_bg").val()+" none repeat scroll 0 0 !important;}";
         var icons='[class^="uiIconPLF24x24"] {background-image: url("/eXoSkin/skin/images/themes/default/platform/skin/Icons/uiIconsPLF24x24'+$('input[name=icons]:checked').val()+'.png") !important;}';
-
-        var style = [topHeadBgLight, topHeadBgDark, topTitleColorLight, topTitleColorDark, pageBg, title, LeftNavigationTDContainer, icons];
+        var leftIcons='.LeftNavigationTDContainer [class^="uiIcon"] {background-image: url("/eXoSkin/skin/images/themes/default/Icons/uiIcons'+$('input[name=leftIcons]:checked').val()+'.png") !important;}';
+        var style = [topHeadBgLight, topHeadBgDark, topTitleColorLight, topTitleColorDark, pageBg, title, LeftNavigationTDContainer, icons, leftIcons, topHeadBgHoDark, topHeadBgHoLight];
 
 
         $.ajax
@@ -71,6 +75,8 @@ $(document).ready(function () {
     $("#top_head_cl").colorpicker({
         color: $('.UIToolbarContainerDark .uiDropdownWithIcon > a').css('color')
     });
+    $("#top_head_ho_cl").colorpicker();
+    $("#top_head_ho_bg").colorpicker();
     $("#page_bg").colorpicker({
        color: $('.RightBodyTDContainer').css('background-color')
     });
@@ -87,8 +93,10 @@ $(document).ready(function () {
 
     var backIcon=$("i[class^='uiIconPLF24x24']").css("background-image");
     var iconSet=backIcon.substring(backIcon.indexOf("uiIconsPLF24x24")+15, backIcon.indexOf(".png"));
-
+    var leftBackIcon=$(".LeftNavigationTDContainer [class^='uiIcon']").css("background-image");
+    var leftIconSet=leftBackIcon.substring(leftBackIcon.indexOf("uiIcons")+7, leftBackIcon.indexOf(".png"));
     $("[name=icons]").val([iconSet]);
+    $("[name=leftIcons]").val([leftIconSet]);
 
     $("#top_head_bg").on("change.color", function(event, color){
         $('.UIToolbarContainerDark .NormalContainerBlock .ToolbarContainer').attr('style', 'background-color: '+$("#top_head_bg").val()+' ! important; cbackground-image: none ! important;');
@@ -102,6 +110,20 @@ $(document).ready(function () {
         $('.UIToolbarContainerLight .uiDropdownWithIcon > a').attr('style', 'color: '+$("#top_head_cl").val()+' ! important;');
 
     })
+
+    $("#top_head_ho_bg").on("change.color", function(event, color){
+        $('.UIToolbarContainerDark .NormalContainerBlock .ToolbarContainer').attr('style', 'background-color: '+$("#top_head_ho_bg").val()+' ! important; cbackground-image: none ! important;');
+        $('.UIToolbarContainerLight .NormalContainerBlock .ToolbarContainer').attr('style', 'background-color: '+$("#top_head_ho_bg").val()+' ! important; cbackground-image: none ! important;');
+
+    })
+
+
+    $("#top_head_ho_cl").on("change.color", function(event, color){
+        $('.UIToolbarContainerDark .uiDropdownWithIcon > a').attr('style', 'color: '+$("#top_head_cl").val()+' ! important;');
+        $('.UIToolbarContainerLight .uiDropdownWithIcon > a').attr('style', 'color: '+$("#top_head_cl").val()+' ! important;');
+
+    })
+
 
     $("#page_bg").on("change.color", function(event, color){
         $('.RightBodyTDContainer').attr('style', 'background: '+$("#page_bg").val()+' none repeat scroll left top ! important;');
@@ -120,11 +142,15 @@ $(document).ready(function () {
 
 
     $("#l_nav_bg").on("change.color", function(event, color){
-        $('.LeftNavigationTDContainer').attr('style', 'background-color: '+$("#l_nav_bg").val()+' none repeat scroll 0 0 !important');
+        $('.LeftNavigationTDContainer').attr('style', 'background: '+$("#l_nav_bg").val()+' none repeat scroll 0 0 !important');
     })
 
     $('input[name=icons]').on("change", function(){
         $("i[class^='uiIconPLF24x24']").attr('style', 'background-image: url("/eXoSkin/skin/images/themes/default/platform/skin/Icons/uiIconsPLF24x24'+$('input[name=icons]:checked').val()+'.png") !important');
+    })
+
+    $('input[name=leftIcons]').on("change", function(){
+        $(".LeftNavigationTDContainer [class^='uiIcon']").attr('style', 'background-image: url("/eXoSkin/skin/images/themes/default/Icons/uiIcons'+$('input[name=leftIcons]:checked').val()+'.png") !important');
     })
 
 });
